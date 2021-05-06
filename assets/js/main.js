fcf1e378c597e1b9bf135a9a7f378efca664f6ef
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2021-05-06 16:23:33 +0200

    add api call

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 (() => {
     console.log('JS works!')
+    fetch(
+        'https://api.monorg.com/all'
+    ).then(response => {
+        const result = response.json()
+    }).then((result) => {
+        console.log('Voilà les données');
+    })
 })()
