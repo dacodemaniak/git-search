8feb0a456f0f26bf91368a874030dd4c01a3de4a
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2021-05-07 15:06:39 +0200

    Refactor main for api class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,4 @@
 (() => {
     console.log('JS works!')
-    fetch(
-        'https://api.monorg.com/all'
-    ).then(response => {
-        const result = response.json()
-        return result
-    }).then((result) => {
-        console.log('Voilà les données')
-    }).catch((error) => {
-        console.log(`Something went wrong while fetching API end point`)
-    })
+
 })()
